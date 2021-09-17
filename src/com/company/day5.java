c5dedc6e7df99032d3b03c144cc5ad14f14db6c3
Rhea Dsouza <rheadqs@gmail.com>
2021-09-17 11:54:39 +0530

    Day5

---
+++ src/com/company/day5.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class day5 {
+
+    public static void main(String[] args) {
+        char[] copyFrom = {'d', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a', 't', 'e', 'd'};
+        char[] copyTo = new char[7];
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        System.out.println(new String(copyTo));
+    }
+}
