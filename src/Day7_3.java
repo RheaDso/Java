019a962993dd903b27c49d774a73b9c92722b793
Rhea Dsouza <rheadqs@gmail.com>
2021-09-19 10:21:47 +0530

    Day7

---
+++ src/Day7_3.java
@@ -0,0 +1,17 @@
+public class Day7_3 {
+
+    public static void main(String[] args)
+    {
+        int a=1;
+        int b=2;
+        int c=3;
+
+        a+=5;
+        b*=4;
+        c+=a*b;
+        c%=6;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+    }
+}
