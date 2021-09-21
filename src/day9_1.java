19c3fcd62f8e9be93610f127df846353d3305f99
Rhea Dsouza <rheadqs@gmail.com>
2021-09-21 23:41:14 +0530

    day9

---
+++ src/day9_1.java
@@ -0,0 +1,13 @@
+public class day9_1 {
+
+    public static void main(String[] args)
+    {
+        int i=1;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
