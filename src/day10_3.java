419f6c0c389ff2395bafc7c85ea7a088815927f4
Rhea Dsouza <rheadqs@gmail.com>
2021-09-22 23:40:27 +0530

    day10

---
+++ src/day10_3.java
@@ -0,0 +1,10 @@
+public class day10_3 {
+
+    public static void main(String[] args)
+    {
+        int arr[]={1,4,5,23,56,10};
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
