69d56e52e33ba032bda6674d98dfc191f1441390
Rhea Dsouza <rheadqs@gmail.com>
2021-09-23 22:46:56 +0530

    day11

---
+++ src/day11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day11 {
+
+    public static void main(String[] args)
+    {
+        Scanner readme = new Scanner(System.in);
+        System.out.println("Enter two number(Press enter after each digit");
+        double n1, n2, n3;
+        n1 = readme.nextDouble();
+        n2 = readme.nextDouble();
+        n3=n1+n2;
+        System.out.println("Total= "+n3);
+    }
+}
