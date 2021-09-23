eb6029033e08c99dde6e524d2336f5e4b9af5740
Rhea Dsouza <rheadqs@gmail.com>
2021-09-23 23:19:44 +0530

    Day11

---
+++ src/day11_5.java
@@ -0,0 +1,14 @@
+public class day11_5 {
+
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            if(i==5)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
