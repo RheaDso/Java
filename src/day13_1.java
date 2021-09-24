798e7b419219fa4aff344c9f2277b54d19e9a717
Rhea Dsouza <rheadqs@gmail.com>
2021-09-25 00:40:02 +0530

    Day13

---
+++ src/day13_1.java
@@ -0,0 +1,9 @@
+public class day13_1 {
+
+    public static void main(String[] args) {
+        double x=34;
+        double y=19;
+        System.out.println("Log1p of x: "+Math.log1p(x));
+        System.out.println("Log1p of x: "+Math.log1p(y));
+    }
+}
