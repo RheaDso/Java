dc68b3c8564774b665902724920707cf194f61ca
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 22:02:14 +0530

    day12

--- src/day12_9.py
+++ src/day12_9.py
@@ -1,11 +1,13 @@
-num=int(input("Enter a number:"))
+import math
+value=int(input())
+num=abs(value)
 temp=num
 rev=0
-while(num>0):
+while(num > 0):
     digit=num%10
     rev=rev*10+digit
     num=num//10
 if(temp==rev):
-    print("The number is palindrome!")
+    print("true")
 else:
-    print("Not a palindrome!")
+    print("false")
