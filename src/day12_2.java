1d094de586a5c026ef6e6a5b8e143bee6e72bd79
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 16:41:15 +0530

    Day12

---
+++ src/day12_2.java
@@ -0,0 +1,52 @@
+public class day12_2 {
+
+    public static void main(String[] args)
+    {
+        char ch='h';
+        switch(ch)
+        {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+
+            case 'e':
+                System.out.println("Vowel");
+                break;
+
+            case 'i':
+                System.out.println("Vowel");
+                break;
+
+            case 'o':
+                System.out.println("Vowel");
+                break;
+
+            case 'u':
+                System.out.println("Vowel");
+                break;
+
+            case'A':
+                System.out.println("Vowel");
+                break;
+
+            case'E':
+                System.out.println("Vowel");
+                break;
+
+            case'I':
+                System.out.println("Vowel");
+                break;
+
+            case'O':
+                System.out.println("Vowel");
+                break;
+
+            case'U':
+                System.out.println("Vowel");
+                break;
+
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
