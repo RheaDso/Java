93c02fc5eaa30524f6f66d87b3140eb00cdc8f3a
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 22:55:26 +0530

    Day12

---
+++ src/day12_9.java
@@ -0,0 +1,8 @@
+public class day12_9 {
+
+    public static void main(String[] args) {
+        double x=34;
+        double y=19;
+        System.out.println("Power of x and y : "+Math.pow(x,y));
+    }
+}
