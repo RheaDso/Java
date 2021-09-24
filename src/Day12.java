6fdec99ac16e62ab24d4c2e83177b4abb3a28906
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 15:44:06 +0530

    Day12

---
+++ src/Day12.java
@@ -0,0 +1,24 @@
+public class Day12 {
+
+    public static void main(String[] args)
+    {
+        int number=20;
+        switch(number)
+        {
+            case 10:
+                System.out.println("10");
+                break;
+
+            case 20:
+                System.out.println("20");
+                break;
+
+            case 30:
+                System.out.println("30");
+                break;
+
+            default:
+                System.out.println("Not in 110,20,30.");
+        }
+    }
+}
