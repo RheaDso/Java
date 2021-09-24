d97a77330516dec0c372883b1cf1683dd566d6c4
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 22:49:55 +0530

    Day12

---
+++ src/day12_7.java
@@ -0,0 +1,10 @@
+public class Day12_7 {
+
+    public static void main(String[] args)
+    {
+        double x=28;
+        double y=10;
+        System.out.println("Maximum number of x and y : "+Math.max(x,y));
+    }
+}
+
