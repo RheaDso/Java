6ce62016c279ecd6b6500e1d3eb418bbc16394e4
Rhea Dsouza <rheadqs@gmail.com>
2021-09-25 00:35:07 +0530

    Day13

---
+++ src/day13.java
@@ -0,0 +1,9 @@
+public class day13 {
+
+    public static void main(String[] args) {
+        double x=34;
+        double y=19;
+        System.out.println("Log10 of x: "+Math.log10(x));
+        System.out.println("Log10 of x: "+Math.log10(y));
+    }
+}
