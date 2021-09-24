74b8c65b103d19360a5d1ec174c85d9254e84db4
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 17:03:47 +0530

    Day12

---
+++ src/day12_4.java
@@ -0,0 +1,9 @@
+public class day12_4 {
+
+    public static void main(String[] args)
+    {
+        int arr[]={33,3,4,5};
+                for(int i:arr)
+                    System.out.println(i);
+    }
+}
