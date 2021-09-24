d3a6c0c47683381d7ec322ccb1bb05aaa9c77c63
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 18:49:42 +0530

    day12

--- src/day12_9.py
+++ src/day12_9.py
@@ -2,8 +2,8 @@ num=int(input("Enter a number:"))
 temp=num
 rev=0
 while(num>0):
-    dig=num%10
-    rev=rev*10+dig
+    digit=num%10
+    rev=rev*10+digit
     num=num//10
 if(temp==rev):
     print("The number is palindrome!")
