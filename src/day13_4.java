74a3572ce8ee3721a27786805bb0fe4ebb5c179d
Rhea Dsouza <rheadqs@gmail.com>
2021-09-25 00:53:50 +0530

    Day13

---
+++ src/day13_4.java
@@ -0,0 +1,9 @@
+public class day13_4 {
+
+    public static void main(String[] args) {
+        double x=39;
+        double y=11;
+        System.out.println("Expm1 of x: "+Math.expm1(x));
+        System.out.println("Expm1 of x: "+Math.expm1(y));
+    }
+}
