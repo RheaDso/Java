c05843a66b4851740f8fa375d59d18dcf6d8d9f7
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 16:53:58 +0530

    Day12

---
+++ src/Day12_3.java
@@ -0,0 +1,9 @@
+public class Day12_3 {
+
+    public static void main(String[] args)
+    {
+        int a[]={33,3,4,5};
+        for(int i=0; i<a.length; i++)
+            System.out.println(a[i]);
+    }
+}
