18ce999711d40128e09181e2141e4b37e82880de
Rhea Dsouza <rheadqs@gmail.com>
2021-09-25 00:49:07 +0530

    Day13

---
+++ src/day13_3.java
@@ -0,0 +1,9 @@
+public class day13_3 {
+
+    public static void main(String[] args) {
+        double x=39;
+        double y=11;
+        System.out.println("Exp of x: "+Math.exp(x));
+        System.out.println("Exp of x: "+Math.exp(y));
+    }
+}
