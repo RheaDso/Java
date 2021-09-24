2874f18be3ae886cf0415015a6988166660af33e
Rhea Dsouza <rheadqs@gmail.com>
2021-09-24 22:52:56 +0530

    Day12

---
+++ src/Day12_6.java
@@ -0,0 +1,10 @@
+public class Day12_7 {
+
+    public static void main(String[] args)
+    {
+        double x=34;
+        double y=19;
+        System.out.println("Square of x and y : "+Math.sqrt(y));
+    }
+}
+
