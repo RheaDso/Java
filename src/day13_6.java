1108246ef4b4f860d3dd1f2a4ea57943af702594
Rhea Dsouza <rheadqs@gmail.com>
2021-09-25 23:17:01 +0530

    Day13

---
+++ src/day13_6.java
@@ -0,0 +1,16 @@
+public class day13_6 {
+
+    public static void main(String[] args)
+    {
+        int arr[][]={{1,2,3},{2,3,4},{4,4,5}};
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                System.out.println(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
