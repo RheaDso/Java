8b4d19afba47c88fb83b075e223509507cb8310f
Rhea Dsouza <rheadqs@gmail.com>
2021-09-25 23:12:07 +0530

    Day13

---
+++ src/day13_5.java
@@ -0,0 +1,11 @@
+public class day13_5 {
+
+    public static void main(String[] args)
+    {
+        int arr[]={50,60,70,80};
+        for(int i=0; i<=arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
