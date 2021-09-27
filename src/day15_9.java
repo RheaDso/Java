deaa98e4c48d59fe537e877b9aa14489454af264
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 16:17:29 +0530

    Day15

---
+++ src/day15_9.java
@@ -0,0 +1,13 @@
+public class day15_9 {
+String name = "Katherine";
+int number = 14;
+String address = "Ohio";
+    public static void main(String[] args)
+    {
+        day15_9 K = new day15_9();
+
+        System.out.println(K.name);
+        System.out.println(K.number);
+        System.out.println(K.address);
+    }
+}
