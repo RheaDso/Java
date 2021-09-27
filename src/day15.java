b2c3e04c79db5b287d3baa1c670e24685f85cb4b
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 09:40:04 +0530

    Day15

---
+++ src/day15.java
@@ -0,0 +1,9 @@
+public class day15 {
+
+    public static void main(String[] args)
+    {
+        int iOb = 100;
+        int i = iOb;
+        System.out.println(i+" "+iOb); //displays 100 100
+    }
+}
