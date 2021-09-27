77a802d08d537692586dd28ff69105449847d8c5
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 17:26:04 +0530

    Day15

---
+++ src/day15_10.java
@@ -0,0 +1,12 @@
+public class day15_10 {
+String Wing = "C";
+int FlatNo = 406;
+String Building="Oceanic";
+    public static void main(String[] args) {
+        day15_10 b = new day15_10();
+
+        System.out.println(b.Wing);
+        System.out.println(b.FlatNo);
+        System.out.println(b.Building);
+    }
+}
