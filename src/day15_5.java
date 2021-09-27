56196f5a7f0ffd047121afd6dbca3f26abcc19c2
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 14:09:38 +0530

    Day15

---
+++ src/day15_5.java
@@ -0,0 +1,16 @@
+public class day15_5 {
+String name="Caroline";
+String surname="Forbes";
+String city="Mystic Falls";
+int age=16;
+
+    public static void main(String[] args)
+    {
+        day15_5 G1 = new day15_5();
+
+        System.out.println(G1.name);
+        System.out.println(G1.surname);
+        System.out.println(G1.city);
+        System.out.println(G1.age);
+    }
+}
