654c7c79ba060c9a409bfa8cb4d416f943368344
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 15:42:17 +0530

    Day15

---
+++ src/day15_7.java
@@ -0,0 +1,11 @@
+public class day15_7 {
+    int number=169;
+    int Number=567;
+
+    public static void main(String[] args)
+    {
+        day15_7 N1 = new day15_7();
+        System.out.println(N1.number);
+        System.out.println(N1.Number);
+    }
+}
