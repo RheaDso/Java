fa56e1d4f24abe52e50799d9bc0c29c924e09abc
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 13:52:11 +0530

    Day15

---
+++ src/day15_4.java
@@ -0,0 +1,15 @@
+public class day15_4 {
+    String name="Rhea Dsouza";
+    int age=19;
+    String college = "Don Bosco Institute of Technology";
+    String Branch = "Information Technology";
+
+    public static void main(String[] args) {
+        day15_4 S1 = new day15_4();
+
+        System.out.println(S1.name);
+        System.out.println(S1.age);
+        System.out.println(S1.college);
+        System.out.println(S1.Branch);
+    }
+}
