01c75a7bf9972a47e48377dc2afd3e0eede8ddbd
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 12:17:19 +0530

    Day15

---
+++ src/day15_3.java
@@ -0,0 +1,12 @@
+public class day15_3 {
+    String name = "Elena";
+    int id=16;
+
+    public static void main(String[] args)
+    {
+        day15_3 E1 = new day15_3();
+
+        System.out.println(E1.name);
+        System.out.println(E1.id);
+    }
+}
