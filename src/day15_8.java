78aea4115279af39a32b305e5dd6e6f0fd270272
Rhea Dsouza <rheadqs@gmail.com>
2021-09-27 15:55:40 +0530

    Day15

---
+++ src/day15_8.java
@@ -0,0 +1,12 @@
+public class day15_8 {
+    String topic = "Shopping Mart";
+    int items = 15;
+
+    public static void main(String[] args)
+    {
+        day15_8 T = new day15_8();
+
+        System.out.println(T.topic);
+        System.out.println(T.items);
+    }
+}
