7565f29b42b8945e96ff663e25eeb6a6cc0441fa
Rhea Dsouza <rheadqs@gmail.com>
2021-09-28 21:36:34 +0530

    Day16

---
+++ src/day16_2.java
@@ -0,0 +1,14 @@
+public class day16_2 {
+
+    public static void main(String[] args) {
+        int age = 16;
+        if (age>18)
+        {
+            System.out.println("Adult");
+        }
+        else
+        {
+            System.out.println("Child");
+        }
+    }
+}
