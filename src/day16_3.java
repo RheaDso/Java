3350b35cc198723c088e2eb772bc6929a10f9a73
Rhea Dsouza <rheadqs@gmail.com>
2021-09-28 21:45:03 +0530

    Day16

---
+++ src/day16_3.java
@@ -0,0 +1,10 @@
+public class day16_3 {
+
+    public static void main(String[] args) {
+        int date=28;
+        if(date>20)
+        {
+            System.out.println("Correct");
+        }
+    }
+}
