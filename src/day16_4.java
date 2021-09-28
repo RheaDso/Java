e2aec67ef407152946dd69b7698b92c03ecc0683
Rhea Dsouza <rheadqs@gmail.com>
2021-09-28 21:53:57 +0530

    Day16

---
+++ src/day16_4.java
@@ -0,0 +1,10 @@
+public class day16_4 {
+
+    public static void main(String[] args) {
+        int number = 6;
+        if(number>1)
+        {
+            System.out.println("Correct");
+        }
+    }
+}
