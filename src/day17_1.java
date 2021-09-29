9f2b075da7a657624024b3836fbc004dcdb00679
Rhea Dsouza <rheadqs@gmail.com>
2021-09-29 12:00:27 +0530

    Day17

---
+++ src/day17_1.java
@@ -0,0 +1,8 @@
+public class day17_1 {
+
+    public static void main(String[] args) {
+        int a=567, b=983, sum;
+        sum = a + b;
+        System.out.println("Sum of the numbers is : "+sum);
+    }
+}
