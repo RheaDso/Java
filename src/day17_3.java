6352de0cc977bf70b233abcc075bff53a8f2876f
Rhea Dsouza <rheadqs@gmail.com>
2021-09-29 12:49:09 +0530

    Day17

---
+++ src/day17_3.java
@@ -0,0 +1,8 @@
+public class day17_3 {
+
+    public static void main(String[] args) {
+        int a=5, b=9, c=6, ans;
+        ans = a + b - c;
+        System.out.println("Result = "+ans);
+    }
+}
