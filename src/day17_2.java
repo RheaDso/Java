06ff8834982f9067f34aa510ff9ecd9f2df4574a
Rhea Dsouza <rheadqs@gmail.com>
2021-09-29 12:10:39 +0530

    Day17

---
+++ src/day17_2.java
@@ -0,0 +1,9 @@
+public class day17_2 {
+
+    public static void main(String[] args) {
+        int a=98, b=65, c=87, sum;
+        sum = a + b + c;
+        System.out.println("Total = "+sum);
+
+    }
+}
