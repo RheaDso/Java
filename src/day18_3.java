94ad647fdc9f8e21d84cf4d06573a88df8978e90
Rhea Dsouza <rheadqs@gmail.com>
2021-09-30 22:28:17 +0530

    Day18

---
+++ src/day18_3.java
@@ -0,0 +1,8 @@
+public class day18_3 {
+
+    public static void main(String[] args) {
+        int a=9, b=7, c=3, mult;
+        mult = a * b * c;
+        System.out.println("Multiplication of the numbers is "+mult);
+    }
+}
