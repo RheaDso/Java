a85a097998d466ffad3efd23e29b31721d651024
Rhea Dsouza <rheadqs@gmail.com>
2021-10-01 21:48:57 +0530

    Day19

---
+++ src/day19.java
@@ -0,0 +1,9 @@
+public class day19 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        float f = a;
+        System.out.println(a);
+        System.out.println(f);
+    }
+}
