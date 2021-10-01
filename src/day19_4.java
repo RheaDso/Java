9a99ab338e9a97e6d1aa52fc5733bb1c54387b86
Rhea Dsouza <rheadqs@gmail.com>
2021-10-01 22:37:56 +0530

    Day19

---
+++ src/day19_4.java
@@ -0,0 +1,9 @@
+public class day19_4 {
+
+    public static void main(String[] args) {
+        int a=130;
+        byte b=(byte)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
