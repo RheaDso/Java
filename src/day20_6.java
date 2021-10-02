fce2939c9572b405e852a4b8497085bab7eb2c27
Rhea Dsouza <rheadqs@gmail.com>
2021-10-02 23:56:24 +0530

    Day20

---
+++ src/day20_6.java
@@ -0,0 +1,8 @@
+public class day20_6 {
+
+    public static void main(String[] args) {
+        int a=5, b=7, c=9, d=2, e=3, ans;
+        ans = a+b-(c*d)/e;
+        System.out.println(""+ans);
+    }
+}
