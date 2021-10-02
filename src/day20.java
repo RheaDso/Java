ed36c13f2013d8b9aee9982b14482848111d4a08
Rhea Dsouza <rheadqs@gmail.com>
2021-10-02 22:22:29 +0530

    Day20

---
+++ src/day20.java
@@ -0,0 +1,14 @@
+public class day20 {
+String name = "Katherine";
+int number = 67;
+String address = "Ohio";
+
+    public static void main(String[] args)
+    {
+        day20 P = new day20();
+
+        System.out.println(P.name);
+        System.out.println(P.number);
+        System.out.println(P.address);
+    }
+}
