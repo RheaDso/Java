bc5a3f830df7f30e26c615ec140152a28570bedd
Rhea Dsouza <rheadqs@gmail.com>
2021-10-02 23:22:45 +0530

    Day20

--- src/day20_2.java
+++ src/day20_2.java
@@ -1,9 +1,8 @@
 public class day20_2 {
 
     public static void main(String[] args) {
-        char[] copyFrom = {'d', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a', 't', 'e', 'd'};
-        char[] copyTo = new char[7];
-        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
-        System.out.println(new String(copyTo));
+        int i=9;
+        for (i=1; i<=10; i++)
+            System.out.println(i);
     }
 }
