25d2aa9767277f64e17ca4ab72daaeb091bdfc0a
Rhea Dsouza <rheadqs@gmail.com>
2021-10-05 12:52:24 +0530

    Day23

---
+++ src/day23_0.java
@@ -0,0 +1,14 @@
+public class day23_0 {
+
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=100; i++)
+
+            if (i%3==0)
+                System.out.println("Rhea"+i);
+            else if(i%5==0)
+                System.out.println("Dsouza"+i);
+            else
+                System.out.println(""+i);
+    }
+}
