b08fdc32f7615978bdbb5934d8c69d17efc291b2
Rhea Dsouza <rheadqs@gmail.com>
2021-10-18 11:21:17 +0530

    Day35

---
+++ src/day35.java
@@ -0,0 +1,10 @@
+public class day35 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        for(x=x+1; x%10!=1; x=x+2)
+        {
+            System.out.println("x" +x);
+        }
+    }
+}
